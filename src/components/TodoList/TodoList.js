5908b882c0d9cdde5bba6473779452d169a60418
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-30 13:21:21 -0300

    Add sort to change when priority is changed

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -13,58 +13,62 @@ const TodoList = () => {
   const dispatch = useDispatch();
 
   const colorPriority = (priority) =>
-    priority === 'low'
+    priority === '1'
       ? { backgroundColor: '#A6F4DC' }
-      : priority === 'medium'
+      : priority === '2'
       ? { backgroundColor: '#f48c06' }
-      : priority === 'hight'
+      : priority === '3'
       ? { backgroundColor: '#d00000' }
       : { backgroundColor: 'transparent' };
 
   return (
     <>
-      {todos.map((t) => {
-        return (
-          <div className="cardList" key={t.id}>
-            <div className="flexRow">
-              <div
-                className="tagPriority"
-                style={colorPriority(t.priority)}
-              ></div>
-              <input
-                type="checkbox"
-                name="check"
-                id="todo"
-                checked={t.done ? true : false}
-                onChange={() => dispatch(checkTodo(t.id))}
-              />
-              <label className="taskName" htmlFor="todo">
-                {t.task}
-              </label>
+      {todos
+        .sort((a, b) => b.priority - a.priority)
+        .map((t) => {
+          return (
+            <div className="cardList" key={t.id}>
+              <div className="flexRow">
+                <div
+                  className="tagPriority"
+                  style={colorPriority(t.priority)}
+                ></div>
+                <input
+                  type="checkbox"
+                  name="check"
+                  id="todo"
+                  checked={t.done ? true : false}
+                  onChange={() => dispatch(checkTodo(t.id))}
+                />
+                <label className="taskName" htmlFor="todo">
+                  {t.task}
+                </label>
+              </div>
+              <div className="btnActions">
+                <select
+                  value={t.priority}
+                  onChange={(e) =>
+                    dispatch(changePriority(t.id, e.target.value))
+                  }
+                  className="select"
+                  name="priority"
+                  id="priority"
+                >
+                  <option>...</option>
+                  <option value="1">Low</option>
+                  <option value="2">Medium</option>
+                  <option value="3">Hight</option>
+                </select>
+                <button
+                  className="btnRemove"
+                  onClick={() => dispatch(removeTodo(t.id))}
+                >
+                  X
+                </button>
+              </div>
             </div>
-            <div className="btnActions">
-              <select
-                value={t.priority}
-                onChange={(e) => dispatch(changePriority(t.id, e.target.value))}
-                className="select"
-                name="priority"
-                id="priority"
-              >
-                <option>...</option>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="hight">Hight</option>
-              </select>
-              <button
-                className="btnRemove"
-                onClick={() => dispatch(removeTodo(t.id))}
-              >
-                X
-              </button>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </>
   );
 };
